8e5afa7b8f39118105761ad44b5a28997f056ab1
MartyL <MartyL@MARTYHOME>
2016-08-11 14:31:17 -0700

    Add project files.

---
+++ src/VS2015-Hello/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VS2015_Hello
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
