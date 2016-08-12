9020a89a77344991e5556462184ae186109923e0
Marty Landa <martyl@mjlmicro.com>
2016-08-12 15:16:45 -0700

    Added Multiplication

--- src/VS2015-Hello/Program.cs
+++ src/VS2015-Hello/Program.cs
@@ -12,6 +12,15 @@ namespace VS2015_Hello
             Console.Write("Please Enter Your Name: ");
             string name = Console.ReadLine();
             Console.WriteLine("Hello " + name + ". It's nice to meet you!");
+            string strNum = "";
+            decimal num = 0;
+            while (decimal.TryParse(strNum, out num) != true)
+            {
+                Console.Write("Enter a Number That I Can Multiply By 10: ");
+                strNum = Console.ReadLine();
+            };
+            num = Convert.ToDecimal(strNum) * 10;
+            Console.WriteLine(strNum + " Times 10 = " + Convert.ToString(num));
             Console.ReadLine();
         }
     }
